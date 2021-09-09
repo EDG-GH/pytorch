da73b3a17afb760fd1f073a19321a317eaac0a11
anjali411 <chourdiaanjali123@gmail.com>
2021-09-09 11:38:07 -0700

    Skip conjugate and negate fallback for view ops and their in-place versions
    ghstack-source-id: 42d535108c4c6487a79d9f09d946512f5c710637
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64392

--- aten/src/ATen/ConjugateFallback.cpp
+++ aten/src/ATen/ConjugateFallback.cpp
@@ -50,12 +50,7 @@ TORCH_LIBRARY_IMPL(aten, Conjugate, m) {
   m.impl("size.int", torch::CppFunction::makeFallthrough());
   m.impl("size.Dimname", torch::CppFunction::makeFallthrough());
   m.impl("is_complex", torch::CppFunction::makeFallthrough());
-  m.impl("view_as_real", torch::CppFunction::makeFallthrough());
-  m.impl("imag", torch::CppFunction::makeFallthrough());
-  m.impl("real", torch::CppFunction::makeFallthrough());
-  m.impl("view", torch::CppFunction::makeFallthrough());
   m.impl("_unsafe_view", torch::CppFunction::makeFallthrough());
-  m.impl("reshape", torch::CppFunction::makeFallthrough());
   m.impl("dot", torch::CppFunction::makeFallthrough());
   m.impl("vdot", torch::CppFunction::makeFallthrough());
   m.impl("dot.out", torch::CppFunction::makeFallthrough());
@@ -71,6 +66,51 @@ TORCH_LIBRARY_IMPL(aten, Conjugate, m) {
   m.impl("baddbmm", torch::CppFunction::makeFallthrough());
   m.impl("baddbmm_", torch::CppFunction::makeFallthrough());
   m.impl("baddbmm.out", torch::CppFunction::makeFallthrough());
+    // views and their in-place version ops
+  m.impl("as_strided", torch::CppFunction::makeFallthrough());
+  m.impl("as_strided_", torch::CppFunction::makeFallthrough());
+  m.impl("detach", torch::CppFunction::makeFallthrough());
+  m.impl("detach_", torch::CppFunction::makeFallthrough());
+  m.impl("diagonal", torch::CppFunction::makeFallthrough());
+  m.impl("expand", torch::CppFunction::makeFallthrough());
+  m.impl("expand_as", torch::CppFunction::makeFallthrough());
+  m.impl("movedim.int", torch::CppFunction::makeFallthrough());
+  m.impl("movedim.intlist", torch::CppFunction::makeFallthrough());
+  m.impl("narrow", torch::CppFunction::makeFallthrough());
+  m.impl("permute", torch::CppFunction::makeFallthrough());
+  m.impl("select.Dimname", torch::CppFunction::makeFallthrough());
+  m.impl("select.int", torch::CppFunction::makeFallthrough());
+  m.impl("squeeze", torch::CppFunction::makeFallthrough());
+  m.impl("squeeze_", torch::CppFunction::makeFallthrough());
+  m.impl("transpose.int", torch::CppFunction::makeFallthrough());
+  m.impl("transpose.Dimname", torch::CppFunction::makeFallthrough());
+  m.impl("transpose_", torch::CppFunction::makeFallthrough());
+  m.impl("t", torch::CppFunction::makeFallthrough());
+  m.impl("t_", torch::CppFunction::makeFallthrough());
+  m.impl("real", torch::CppFunction::makeFallthrough());
+  m.impl("imag", torch::CppFunction::makeFallthrough());
+  m.impl("view_as_real", torch::CppFunction::makeFallthrough());
+  m.impl("unflatten.int", torch::CppFunction::makeFallthrough());
+  m.impl("unflatten.Dimname", torch::CppFunction::makeFallthrough());
+  m.impl("unfold", torch::CppFunction::makeFallthrough());
+  m.impl("unsqueeze", torch::CppFunction::makeFallthrough());
+  m.impl("unsqueeze_", torch::CppFunction::makeFallthrough());
+  m.impl("view", torch::CppFunction::makeFallthrough());
+  m.impl("view_as", torch::CppFunction::makeFallthrough());
+  m.impl("unbind.int", torch::CppFunction::makeFallthrough());
+  m.impl("unbind.Dimname", torch::CppFunction::makeFallthrough());
+  m.impl("split.Tensor", torch::CppFunction::makeFallthrough());
+  m.impl("split_with_sizes", torch::CppFunction::makeFallthrough());
+  m.impl("swapaxes", torch::CppFunction::makeFallthrough());
+  m.impl("swapdims", torch::CppFunction::makeFallthrough());
+  m.impl("chunk", torch::CppFunction::makeFallthrough());
+  m.impl("reshape", torch::CppFunction::makeFallthrough());
+  m.impl("hsplit.int", torch::CppFunction::makeFallthrough());
+  m.impl("hsplit.array", torch::CppFunction::makeFallthrough());
+  m.impl("dsplit.int", torch::CppFunction::makeFallthrough());
+  m.impl("dsplit.array", torch::CppFunction::makeFallthrough());
+  m.impl("vsplit.int", torch::CppFunction::makeFallthrough());
+  m.impl("vsplit.array", torch::CppFunction::makeFallthrough());
 }
 
 } // namespace at
